c24ca64e0a334826c465c42632c6e0236a05c793
Lorena Roberta <88508108+lorenarbt@users.noreply.github.com>
2022-06-27 16:46:19 -0300

    comentarios cadastro

--- resources/views/site/cadastro.blade.php
+++ resources/views/site/cadastro.blade.php
@@ -75,21 +75,7 @@
                                     </div>
                                 </div>
                                 <button type="submit" class="btn btn-primary btn-user btn-block">Registrar Conta</button>
-                                {{-- <a href="{{route('cadastro.auth')}}" class="btn btn-primary btn-user btn-block">
-                                    Registrar Conta
-                                </a> --}}
                                 <hr>
-                                <!-- {{-- <a href="index.html" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Register with Google
-                                </a>
-                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                                </a> --}} -->
-                            </form>
-                            {{-- <hr> --}}
-                            <!-- <div class="text-center">
-                                <a class="small" href="forgot-password.html">Esqueceu a senha?</a>
-                            </div> -->
                             <div class="text-center">
                                 <a class="small" href="login">Já possui uma conta? Login!</a>
                             </div>
